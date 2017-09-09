99c294b58738104b56ca7aebd4161b9c81c66b55
happy <505618233@qq.com>
2017-09-09 09:48:42 +0800

    versiom3.0

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,10 @@
 
 Lei L;
 MOUSEMSG mouse;
+<<<<<<< HEAD
+=======
+IMAGE replace, re;
+>>>>>>> version3.0
 int positionx = 0, positiony = 0;
 
 Game::Game()
@@ -27,6 +31,7 @@ void Game::GameStart()
 }
 
 
+<<<<<<< HEAD
 void Game::GameRun()                                                     //游戏运行
 {
 	IMAGE replace,re;
@@ -101,3 +106,86 @@ void Game::GameOver()
 	_getch();               // 按任意键继续
 	closegraph();           // 关闭图形界面
 }
+=======
+void Game::GameRun()                                                //游戏运行
+{	
+	int ClickCount = 0;												//鼠标点击次数	
+	loadimage(&re, _T("./IMAGE/cell.jpg"));
+	mouse = GetMouseMsg();
+	while (1) 
+	{
+		if (mouse.uMsg == WM_LBUTTONDOWN)                           //左键选择
+		{                                                      
+			positionx = mouse.x / Picture_Width;
+			positiony = mouse.y / Picture_Width;
+			Judge(positionx, positiony);
+			if (9 == L.point[positionx][positiony])
+				GameOver_Lose();
+			ClickCount++;
+		}
+		if (Floor_Row * Floor_Col - Mine_Number == ClickCount)
+			GameOver_Win();
+		mouse = GetMouseMsg();
+	}
+}
+
+void Game::GameOver_Win()
+{
+	IMAGE win;
+	loadimage(&win, _T("./IMAGE/icon.jpg"));
+	for (int row = 0; row < Floor_Row; ++row)
+		for (int col = 0; col < Floor_Col; ++col)
+			Judge(row, col);
+
+	Sleep(10000);
+	cleardevice();
+	putimage(5 * Picture_Width, 5 * Picture_Width, &win);
+
+	_getch();               // 按任意键继续
+	closegraph();           // 关闭图形界面
+}
+
+void Game::GameOver_Lose()
+{
+	IMAGE lose;
+	loadimage(&lose, _T("./IMAGE/icon.jpg"));
+
+	for (int row = 0; row < Floor_Row; ++row)
+		for (int col = 0; col < Floor_Col; ++col)
+			Judge(row, col);
+
+	Sleep(10000);
+	cleardevice();
+	putimage(5 * Picture_Width, 5 * Picture_Width, &lose);
+
+	_getch();               // 按任意键继续
+	closegraph();           // 关闭图形界面
+}
+
+void Game::Judge(int row,int col)
+{
+	switch (L.point[row][col])
+	{
+	case 1:loadimage(&replace, _T("./IMAGE/1.jpg"));
+		break;
+	case 2:loadimage(&replace, _T("./IMAGE/2.jpg"));
+		break;
+	case 3:loadimage(&replace, _T("./IMAGE/3.jpg"));
+		break;
+	case 4:loadimage(&replace, _T("./IMAGE/4.jpg"));
+		break;
+	case 5:loadimage(&replace, _T("./IMAGE/5.jpg"));
+		break;
+	case 6:loadimage(&replace, _T("./IMAGE/6.jpg"));
+		break;
+	case 7:loadimage(&replace, _T("./IMAGE/7.jpg"));
+		break;
+	case 8:loadimage(&replace, _T("./IMAGE/8.jpg"));
+		break;
+	case 9:loadimage(&replace, _T("./IMAGE/bomb.jpg"));
+		break;
+	}
+	putimage(row * Picture_Width, col * Picture_Width, &re);
+	putimage(row * Picture_Width, col * Picture_Width, &replace);
+}
+>>>>>>> version3.0
--- floor.cpp
+++ floor.cpp
@@ -18,6 +18,7 @@ Lei::~Lei()														//析构函数
 	
 }
 
+<<<<<<< HEAD
 void Lei::Set_Mine(Cell map[Floor_Row][Floor_Col],int X_Start,int Y_Start)
 {
 	for(int i = 0; i < Floor_Row; i++)
@@ -53,6 +54,42 @@ void Lei::Set_Mine(Cell map[Floor_Row][Floor_Col],int X_Start,int Y_Start)
 
 	
 }
+=======
+//void Lei::Set_Mine()
+//{
+//	for(int i = 0; i < Floor_Row; i++)
+//	{
+//		for (int j = 0; j < Floor_Col; j++)
+//		{
+//			point[i][j].Mine_Num = 0;  
+//			point[i][j].Has_Mine = false;
+//			point[i][j].Is_Dug = false;
+//		}
+//
+//	}
+//	srand((unsigned)time(NULL));										//设置随机数种子
+//	int count = 0;														//设置当前地雷数量
+//	int Mine[Mine_Number];												//设置地雷集（40个地雷）
+//	while(count < Mine_Number)
+//	{
+//		if (0 == count)												
+//			Mine[0] = rand() % (Floor_Row * Floor_Col);					//设置0-256的随机数，公式百度有
+//		else
+//		{
+//			Mine[count] = rand() % (Floor_Row * Floor_Col);
+//		}
+//		count++;
+//	}
+//
+//	for(int i = 0; i < Mine_Number; i++)
+//	{
+//		point[Mine[i] / Floor_Row == 0 ? 0 : Mine[i] / Floor_Row - 1][Mine[i] - 1].Has_Mine = true;		//使用三目运算符，将地雷位置对应到地图中
+//	}
+//						
+//
+//	
+//}
+>>>>>>> version3.0
 
 
 void Lei::Draw_Floor()											
@@ -130,6 +167,7 @@ void Lei::Lei_Travel() {
 		point[0][0] = mark;
 		mark = 0;
 	}
+<<<<<<< HEAD
 	if (point[0][15] != 9) {                           //右上角
 		if (point[0][14] == 9)
 		{
@@ -180,6 +218,58 @@ void Lei::Lei_Travel() {
 	}
 	row = 0; line = 1;                                   //第一行
 	for (; line < 15; line++) {
+=======
+	if (point[0][Floor_Col - 1] != 9) {                           //右上角
+		if (point[0][Floor_Col - 2] == 9)
+		{
+			mark++;
+		}
+		if (point[1][Floor_Col - 2] == 9)
+		{
+			mark++;
+		}
+		if (point[1][Floor_Col - 1] == 9)
+		{
+			mark++;
+		}
+		point[0][Floor_Col - 1] = mark;
+		mark = 0;
+	}
+	if (point[Floor_Row - 1][0] != 9) {                      //左下角
+		if (point[Floor_Row - 2][0] == 9)
+		{
+			mark++;
+		}
+		if (point[Floor_Row - 2][1] == 9)
+		{
+			mark++;
+		}
+		if (point[Floor_Row - 1][1] == 9)
+		{
+			mark++;
+		}
+		point[Floor_Row - 1][0] = mark;
+		mark = 0;
+	}
+	if (point[Floor_Row - 1][Floor_Col - 1] != 9) {                      //右下角
+		if (point[Floor_Row - 2][Floor_Col - 1] == 9)
+		{
+			mark++;
+		}
+		if (point[Floor_Row - 2][Floor_Col - 1] == 9)
+		{
+			mark++;
+		}
+		if (point[Floor_Row - 1][Floor_Col - 1] == 9)
+		{
+			mark++;
+		}
+		point[Floor_Row - 1][Floor_Col - 1] = mark;
+		mark = 0;
+	}
+	row = 0; line = 1;                                   //第一行
+	for (; line < Floor_Col - 1; line++) {
+>>>>>>> version3.0
 		if (point[row][line] != 9) {
 			if (point[row][line - 1] == 9)
 			{
